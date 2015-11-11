f656be85379b5736d17b3faa69065de22529e26a
Tester <majiatester@gmail.com>
2015-11-11 18:20:25 -0500

    Initial

---
+++ Cd.h
@@ -0,0 +1,15 @@
+// base class
+class Cd { // represents a CD disk
+private: 
+	char performers[50];
+	char label[20];
+	int selections; // number of selections
+	double playtime; // playing time in minutes
+public:
+	Cd(char * s1, char * s2, int n, double x);
+	Cd(const Cd &d);
+	Cd();
+	~Cd();
+	void report() const; // reports all the CD data
+	Cd & operator=(const Cd &d);
+};
