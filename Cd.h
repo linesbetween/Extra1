1365a6803e8941f9af5a0c8e6c406b694b99de8a
Tester <majiatester@gmail.com>
2015-11-12 17:19:52 -0500

    Cd Tested
    Cd.h, Cd.cpp, Test.cpp modified

--- Cd.h
+++ Cd.h
@@ -5,6 +5,10 @@ private:
 	char label[20];
 	int selections; // number of selections
 	double playtime; // playing time in minutes
+	//Actual length of performers and label name
+	int sLength1;
+	int sLength2;
+
 public:
 	Cd(char * s1, char * s2, int n, double x);
 	Cd(const Cd &d);
